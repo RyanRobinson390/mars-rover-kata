a584df2cea257b0d7951ec45bd119588b94d42e3
Zach Hodges <zach.hodges@ao.com>
2021-04-30 15:41:32 +0100

    Can move forward & back

--- Tests/Grid.cs
+++ Tests/Grid.cs
@@ -1,11 +1,58 @@
-﻿namespace Tests
+﻿using System.Reflection;
+
+namespace Tests
 {
     public class Grid
     {
-        public int[,] InitializeGrid()
+        public string[,] Area = new string[8, 8];
+        public int[] RoverPosition { get; set; } = new int[2];
+    }
+
+    public class Rover
+    {
+        public int[] Position { get; set; } = new int[2];
+
+        private readonly Grid _grid;
+
+        public Rover(Grid grid)
         {
-            int[,] gridSpace = new int[8, 8];
-            return gridSpace;
+            _grid = grid;
+        }
+        
+        public void Move(string command)
+        {
+            if (command == "F")
+                MoveForward(command);
+
+            if (command == "B")
+                MoveBackward(command);
+        }
+
+        private void MoveBackward(string command)
+        {
+            Position[1]++;
+
+            var x = Position[0];
+            var y = Position[1];
+
+            _grid.Area[x, y] = "R";
+        }
+
+        private void MoveForward(string command)
+        {
+            Position[1]--;
+
+            var x = Position[0];
+            var y = Position[1];
+
+            _grid.Area[x, y] = "R";
+        }
+
+        public void SetRoverPosition(int x, int y)
+        {
+            _grid.Area[x, y] = "R";
+            Position[0] = x;
+            Position[1] = y; 
         }
     }
 }
--- Tests/RoversTests.cs
+++ Tests/RoversTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace Tests
 {
@@ -6,25 +7,48 @@ namespace Tests
     public class RoversTests
     {
         private Grid _grid;
+        private Rover _rover;
 
         [SetUp]
         public void SetUp()
         {
             _grid = new Grid();
+            _rover = new Rover(_grid); 
         }
-
-        [Test]
-        public void Can_GetGrid()
-        {
-            _grid.InitializeGrid();
-        }
-
+        
         [Test]
         public void GetGrid_Initialize_IsEightByEight()
         {
-            var grid = _grid.InitializeGrid();
-
-            Assert.That(grid.Length, Is.EqualTo(64));
+            Assert.That(_grid.Area.Length, Is.EqualTo(64));
         }
+
+        [Test]
+        public void InitGrid_HasRover()
+        {
+            _rover.SetRoverPosition(4, 5);
+            
+            Assert.That(_grid.Area[4, 5], Is.EqualTo("R"));
+       }
+        
+        [Test]
+        public void InitGrid_RoverCanMoveForward()
+        {
+            _rover.SetRoverPosition(4, 5);
+
+            _rover.Move("F");
+
+            Assert.That(_grid.Area[4, 4], Is.EqualTo("R"));
+        }
+        
+        [Test]
+        public void InitGrid_RoverCanMoveBackward()
+        {
+            _rover.SetRoverPosition(4, 4);
+
+            _rover.Move("B");
+
+            Assert.That(_grid.Area[4, 5], Is.EqualTo("R"));
+        }
+        
     }
 }
