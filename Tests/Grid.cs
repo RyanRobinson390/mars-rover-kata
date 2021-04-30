d4939ef97ca08cf94b17eb2b608ea45c2e76eaa9
Ryan Robinson <ryan.robinson@ao.com>
2021-04-30 14:01:34 +0100

    first initial test

---
+++ Tests/Grid.cs
@@ -0,0 +1,11 @@
+﻿namespace Tests
+{
+    public class Grid
+    {
+        public int[,] InitializeGrid()
+        {
+            int[,] gridSpace = new int[8, 8];
+            return gridSpace;
+        }
+    }
+}
---
+++ Tests/RoversTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RoversTests
+    {
+        private Grid _grid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _grid = new Grid();
+        }
+
+        [Test]
+        public void Can_GetGrid()
+        {
+            _grid.InitializeGrid();
+        }
+
+        [Test]
+        public void GetGrid_Initialize_IsEightByEight()
+        {
+            var grid = _grid.InitializeGrid();
+
+            Assert.That(grid.Length, Is.EqualTo(64));
+        }
+    }
+}
